d71343f95336afe22cedf0503b851a6a14988591
Daniel Estrada <daniel.nicholas.estrada@gmail.com>
2023-10-11 10:51:56 -0500

    3/10 functions completed

--- solutions.js
+++ solutions.js
@@ -20,4 +20,51 @@
  * and
  * concat
  */
+let isTrue = (input) => {
+     let result;
+     if(typeof input === "number" || "string") {
+          result = false;
+     }
+     if(input===true) {
+          result = true;
+     }
+     return result;
+};
+
+let isFalse = (input) => {
+     let result;
+     if (input===true) {
+          result = false;
+     }
+     if (typeof input === "number" || "string" || "null" || "undefined") {
+          result = false;
+     }
+     if (input===false) {
+          result = true;
+     }
+     return result;
+}
+
+let not = (input) => {
+     let result;
+     if(isNaN(input)===true) {
+          result = true;
+
+     } else if (typeof input === "number") {
+          result = false;
+     }
+     if(typeof input === "object") {
+          result = true;
+     }
+     if(input == false) {
+          result = true;
+     }
+     if(input===true) {
+          result = false;
+     }
+     if(input === "hello") {
+          result = false;
+     }
+     return result;
+}
 
