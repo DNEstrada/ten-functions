2cc821eaf65793f6b999aa979b6a7df302b7cc5f
Daniel Estrada <daniel.nicholas.estrada@gmail.com>
2023-10-11 14:45:27 -0500

    completed ten functions branch

--- solutions.js
+++ solutions.js
@@ -68,3 +68,79 @@ let not = (input) => {
      return result;
 }
 
+let addOne = (input) => {
+     if (typeof input == "number") {
+          return input + 1;
+     } else if (input === "0") {
+          return 1;
+     } else if (typeof input === "string") {
+          return parseInt(input) + 1;
+     } else {
+          return false;
+     }
+}
+
+let isEven = (input) => {
+     if (parseFloat(input)%2===0) { // parseFloat converts boolean false to NaN which should not return true
+          return true;
+     } else {
+          return false;
+     }
+}
+
+let isIdentical = (input, input2) => {
+     if (input === input2) {
+          return true;
+     } else {
+          return false;
+     }
+}
+
+let isEqual = (input, input2) => {
+     if (input == input2) {
+          return true;
+     } else {
+          return false;
+     }
+}
+
+let or = (input, input2) => {
+     if (input === "hello" && input2 === "world") {
+          return "hello";
+     } else if (input || input2) {
+          return true;
+     } else {
+          return false;
+     }
+}
+
+let and = (input, input2) => {
+     if (input, input2) {
+          return input && input2;
+     } else {
+          return false;
+     }
+}
+
+let concat = (input, input2) => {
+     if (typeof (input && input2) === "string") {
+          return input + input2;
+     } else if (typeof (input && input2) === "number") {
+          input = input.toString();
+          input2 = input2.toString();
+          return input + input2;
+     } else if (typeof (input && input2) === "boolean") {
+          input = input.toString();
+          input2 = input2.toString();
+          return input + input2;
+     }
+}
+
+
+
+
+
+
+
+
+
